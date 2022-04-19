16f767239f856a00a7f6df49fb8369611a71d317
Norah <norayourstar@gmail.com>
2022-04-19 12:51:58 +0300

    basic design

--- app/src/main/java/com/norah/albaqami/daynightadhkar/MainActivity.kt
+++ app/src/main/java/com/norah/albaqami/daynightadhkar/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.norah.albaqami.daynightadhkar
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(getResources().getColor(R.color.black))
+        }
     }
 }
